7c04d60a9db556c02e00562c039f8f2365343595
Romel Niño Paano <rpaano02@gmail.com>
2019-09-09 17:26:42 +0800

    Updated

---
+++ CheckBoxText/Deafult.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Deafult.aspx.cs" Inherits="CheckBoxText.Deafult" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Choose your language please:<br />
+            </h3>
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server" AutoPostBack="True">
+            </asp:CheckBoxList>
+            <br />
+            <br />
+            <asp:Button ID="btnOk" runat="server" OnClick="btnOk_Click" Text="Ok" />
+            <br />
+            <br />
+            <asp:Label ID="lblResult" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ CheckBoxText/Deafult.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CheckBoxText
+{
+    public partial class Deafult : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CheckBoxList1.Items.Add("C");
+                CheckBoxList1.Items.Add("C++");
+                CheckBoxList1.Items.Add("C#");
+                CheckBoxList1.Items.Add("Visual Studio 6.0");
+                CheckBoxList1.Items.Add("VB.NET");
+                CheckBoxList1.Items.Add("Pascal");
+            }
+            //or
+            /*
+            if (Page.IsPostBack == false)
+            {
+                CheckBoxList1.Items.Add("C");
+                CheckBoxList1.Items.Add("C++");
+                CheckBoxList1.Items.Add("C#");
+                CheckBoxList1.Items.Add("Visual Studio 6.0");
+                CheckBoxList1.Items.Add("VB.NET");
+                CheckBoxList1.Items.Add("Pascal");
+            }
+            */
+        }
+
+        protected void btnOk_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "You chose: <b>";
+
+            foreach(ListItem listItem in CheckBoxList1.Items)
+            {
+                if(listItem.Selected == true)
+                {
+                    lblResult.Text += " <br />" + listItem.Text;
+                }
+            }
+
+            lblResult.Text += "</b>";
+        }
+    }
+}
